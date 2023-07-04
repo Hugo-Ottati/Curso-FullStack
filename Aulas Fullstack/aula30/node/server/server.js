89c18f858d92d521c3707397c6238aa1e115d90e
Hyuuga7 <hugo.figueiredo99@gmail.com>
2023-07-04 16:42:01 -0300

    Primeiro Server

---
+++ Aulas Fullstack/aula30/node/server/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const app = express();
+
+const port = process.env.PORT || 7777;
+
+// app.get("/", (req, res) => {
+//     res.send("<h1>Fala Galera! Tranquilidade?</h1>")
+// });
+
+app.use(express.static(__dirname + "./public"));
+
+app.listen(port, () => {
+console.log(`Aplicação rodando no link http://localhost:${port}`);
+});
